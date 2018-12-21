2ec07b1b6bc8d7170bd5a7ce3991c7866e05f492
AntCLEM <antoine.clement.r@gmail.com>
2018-12-21 16:01:06 +0100

    creation de la classe Card et de la afficher

---
+++ card.h
@@ -0,0 +1,19 @@
+#ifndef DEF_CARD
+#define DEF_CARD
+
+#include <string>
+
+class Card
+{
+	private:
+	std::string recto;
+	std::string verso;
+	std::string visibleFace; // Pour savoir quel cote est visible
+	
+	public:
+	Card(std::string rectoCard, std::string versoCard);
+	void displayCard();
+
+};
+
+#endif
