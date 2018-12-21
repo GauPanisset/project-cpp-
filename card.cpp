89aca8053dda9354aa06fab065c84c3a91c4d060
AntCLEM <antoine.clement.r@gmail.com>
2018-12-21 16:26:00 +0100

    Ajout de la fonction swap

--- card.cpp
+++ card.cpp
@@ -1,5 +1,6 @@
 #include "card.h"
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
@@ -12,6 +13,7 @@ Card::Card(string rectoCard, string versoCard)
 
 void Card::displayCard()
 {
+
 	if (visibleFace == "recto")
 	{
 		cout<<recto<<endl;
@@ -20,4 +22,22 @@ void Card::displayCard()
 	{
 		cout<<verso<<endl;
 	}
+	
+	
+}
+
+void Card::swapCard()
+{
+	cout<<"Appuyer sur Entree pour retourner la carte et vérifiez votre réponse";
+	while(getchar() != '\n'); //Attendre que l'utilisateur ai appuye sur Entree pour retourner la carte
+	if (visibleFace == "recto")
+	{
+		visibleFace = "verso";
+	}
+	else
+	{
+		visibleFace = "recto";
+	}
+	Card::displayCard();
+
 }
