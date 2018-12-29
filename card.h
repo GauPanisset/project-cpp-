c2b19bece6df93996c00d2e91d047e43acc326a3
AntCLEM <antoine.clement.r@gmail.com>
2018-12-29 15:19:21 +0100

    ajout de getRecto

--- card.h
+++ card.h
@@ -9,11 +9,12 @@ class Card
 	std::string recto;
 	std::string verso;
 	std::string visibleFace; // Pour savoir quel cote est visible
-	
+
 	public:
 	Card(std::string rectoCard, std::string versoCard);
 	void displayCard(); // Permet d afficher la carte
 	void swapCard(); // Permet de retourner la carte
+	std::string getRecto();
 
 };
 
